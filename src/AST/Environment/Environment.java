50557ba6b19e8941155214bd251dfbd87aa3115a
Rúben <rav.rebelo@campus.fct.unl.pt>
2020-11-05 11:14:19 +0000

    Assoc, beginScope and endScope for Env

--- src/AST/Environment/Environment.java
+++ src/AST/Environment/Environment.java
@@ -1,5 +1,6 @@
 package AST.Environment;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Environment<V> {
@@ -7,15 +8,17 @@ public class Environment<V> {
 	Map <String, V> map;
 	
 	public Environment(Environment<V> p) {
-		
+		this.parent = p;
+		map = new HashMap<String, V>();
 	}
 	
 	public Environment<V> beginScope() {
-		
+		Environment<V> envNew = new Environment<V>(this);
+		return envNew;
 	}
 	
 	public Environment<V> endScope() {
-		
+		return this.parent;
 	}
 	
 	public V find(String Id) {
@@ -23,7 +26,7 @@ public class Environment<V> {
 	}
 	
 	public void assoc (String id, V value) {
-		
+		map.put(id, value);
 	}
 
 }
