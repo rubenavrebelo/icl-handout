95b690e8d8398762b6dfcb58f1b75a9ba1903f65
Rúben <rav.rebelo@campus.fct.unl.pt>
2020-11-05 11:07:04 +0000

    Base generic Environment

--- src/AST/Environment/Environment.java
+++ src/AST/Environment/Environment.java
@@ -1,5 +1,29 @@
 package AST.Environment;
 
-public class Environment {
+import java.util.Map;
+
+public class Environment<V> {
+	Environment<V> parent;
+	Map <String, V> map;
+	
+	public Environment(Environment<V> p) {
+		
+	}
+	
+	public Environment<V> beginScope() {
+		
+	}
+	
+	public Environment<V> endScope() {
+		
+	}
+	
+	public V find(String Id) {
+		
+	}
+	
+	public void assoc (String id, V value) {
+		
+	}
 
 }
