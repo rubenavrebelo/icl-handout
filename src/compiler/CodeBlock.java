b595b144f737905ddb0bb87cd26e7c81806bffab
Rúben <rav.rebelo@campus.fct.unl.pt>
2020-11-12 12:27:13 +0000

    Write into file

--- src/compiler/CodeBlock.java
+++ src/compiler/CodeBlock.java
@@ -1,19 +1,29 @@
 package compiler;
 
 import java.io.PrintStream;
+import java.util.LinkedList;
 import java.util.List;
 
 public class CodeBlock {
 	private List<String> code;
+	
+	public CodeBlock() {
+		code = new LinkedList<>();
+	}
 	
 	public void emit(String bytecode) {
 		code.add(bytecode);
 	}
 	
 	public void dump(PrintStream f) {
-		
 		try {
+			for (String codeBlock : code) {
+				f.println(codeBlock);
+			}
+		} catch (Exception e) {
+			e.getStackTrace();
 		}
+	    f.close();
 	}
 
 }
