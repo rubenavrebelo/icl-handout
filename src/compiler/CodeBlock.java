4a414bf51136d8926d59d190a04be85b462f5520
Rúben <rav.rebelo@campus.fct.unl.pt>
2020-11-16 21:58:43 +0000

    Compile File Start and End

--- src/compiler/CodeBlock.java
+++ src/compiler/CodeBlock.java
@@ -7,6 +7,32 @@ import java.util.List;
 public class CodeBlock {
 	private List<String> code;
 	
+	public static final String COMP_START = ".class public Demo\n" + ".super java/lang/Object\n" + "\n" + ";\n"
+			+ "; standard initializer\n"
+			+ ".method public <init>()V\n" + "   aload_0\n"
+			+ "   invokenonvirtual java/lang/Object/<init>()V\n" + "   return\n" + ".end method\n" + "\r\n"
+			+ ".method public static main([Ljava/lang/String;)V\n" 
+			+ "       ; set limits used by this method\n"
+			+ "       .limit locals 4\n" 
+			+ "       .limit stack 256\n" + "\n"
+			+ "       ; setup local variables:\n" + "\n"
+			+ "       ;    1 - the PrintStream object held in java.lang.System.out\n"
+			+ "       getstatic java/lang/System/out Ljava/io/PrintStream;\n" + "\n"
+			+ "       ; place your bytecodes here\n"
+			+ "       ; START\n" 
+			+ "\n"
+			+ "aconst_null\nastore 4\n";
+	
+	public static final String COMP_END =  
+			"       ;END\n"
+			+ "\n" + "\r\n" 
+			+ "       ; convert to String;\n"
+			+ "       invokestatic java/lang/String/valueOf(I)Ljava/lang/String;\n" 
+			+ "       ; call println \n"
+			+ "       invokevirtual java/io/PrintStream/println(Ljava/lang/String;)V\n" 
+			+ "\n" + "       return\n"
+			+ "\n" + ".end method";
+	
 	public CodeBlock() {
 		code = new LinkedList<>();
 	}
@@ -17,9 +43,11 @@ public class CodeBlock {
 	
 	public void dump(PrintStream f) {
 		try {
+			f.println(COMP_START);
 			for (String codeBlock : code) {
 				f.println(codeBlock);
 			}
+			f.println(COMP_END);
 		} catch (Exception e) {
 			e.getStackTrace();
 		}
